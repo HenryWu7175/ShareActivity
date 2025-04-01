63e7b5ffdb168c30d1758bc6ce3a5d261bb5f830
HenryW7175 <143651504+HenryWu7175@users.noreply.github.com>
2025-04-01 11:58:03 -0400

    changed compile sdk to 35, added the code to handle sharing button

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -15,7 +16,24 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            //checks if the text is not empty
+            if (editText.text.isNotEmpty()) {
+                //create an implicit intent to share the text
+                //action send means to share something
+                val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                    //the type / mime type of the data being shared is text
+                    type = "text/plain" //mime type for text
+                    //the extra text is the text to share
+                    putExtra(Intent.EXTRA_TEXT, editText.text.toString())
+                }
+                //this if statement checks if there is an activity that can handle the share intent
+                if (shareIntent.resolveActivity(packageManager) != null) {
+                    //start the activity that can handle the share intent
+                    //share via is the title of the activity
+                    //chooser is a built in activity that allows the user to choose which app to use to share the text
+                    startActivity(Intent.createChooser(shareIntent, "Share via"))
+                }
+            }
         }
     }
 }
